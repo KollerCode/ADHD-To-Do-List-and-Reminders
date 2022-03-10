e56b574ac35db31c40dc1e36d2a408d93e9ccb79
Natasha Koller <kollernatasha@gmail.com>
2022-03-09 23:14:00 -0500

    started brain dump portion

--- src/App.js
+++ src/App.js
@@ -2,85 +2,30 @@ import React from 'react';
 // import "bootstrap"
 import Header from './components/Header'
 import NavBar from './components/NavBar';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, Routes } from "react-router-dom";
 import ToDoListForm from './components/ToDoListForm';
+import BrainDump from './components/BrainDump'
 
 function App() {
-    return (
-      // <Router>
-      <div>
-        <header>
-          <NavBar />
-          <Header />
-        </header>
-        <div className="main-form">
+  return (
+    <>
+      {/* <Router> */}
+        <div>
+          <header>
+            <NavBar />
+            <Header />
+          </header>
+          {/* <Routes> */}
+            <div className="main-form">
+          {/* <Route path="/" element={<ToDoListForm />} /> */}
           <ToDoListForm />
+            </div>
+        {/* <Route path="/braindump" element={<BrainDump />} /> */}
+          {/* </Routes> */}
         </div>
-      </div>
-
-      /* <Routes>
-            <Route path="/" element={<Home />} />
-
-            <Route path="/about" element={<About />} />
-            <Route path="/mental-health" element={<MentalHealth />} />
-            <Route path="legal" element={<Legal />} />
-            <Route path="/signUp" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/logout" element={<Home />} />
-            <Route path="/community" element={<Culture />} />
-          </Routes> */
-      /* <footer className="App-footer">
-          <Footer />
-        </footer>
-        </Router> */
-    );
+      {/* </Router> */}
+    </>
+  );
 }
 
 export default App
-// export default function App() {
-//   return (
-//     <Router>
-//       <div>
-//         <nav>
-//           <ul>
-//             <li>
-//               <Link to="/">Home</Link>
-//             </li>
-//             <li>
-//               <Link to="/about">About</Link>
-//             </li>
-//             <li>
-//               <Link to="/users">Users</Link>
-//             </li>
-//           </ul>
-//         </nav>
-
-//         {/* A <Switch> looks through its children <Route>s and
-//             renders the first one that matches the current URL. */}
-//         <Switch>
-//           <Route path="/about">
-//             <About />
-//           </Route>
-//           <Route path="/users">
-//             <Users />
-//           </Route>
-//           <Route path="/">
-//             <Home />
-//           </Route>
-//         </Switch>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// function Home() {
-//   return <h2>Home</h2>;
-// }
-
-// function About() {
-//   return <h2>About</h2>;
-// }
-
-// function Users() {
-//   return <h2>Users</h2>;
-// }
--- src/components/BrainDump.js
+++ src/components/BrainDump.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function BrainDump() {
-const getBrainDumps = fetch("/dumps")
-  .then((response) => response.json())
-  .then((data) => console.log(data.dumps));
+  const [description, setDescription] = useState("");
+  // function handleSubmit(e) {
+  //         e.preventDefault();
+  //         };
+const getBrainDumps = 
+  fetch("http://localhost:4000/dumps", {
+             method: "POST",
+             headers: {
+               "Content-Type": "application/json",
+             },
+    body: JSON.stringify({ description: description }),
+           })
+             .then((r) => r.json())
+            //  .then((data) => onAddDumps(data));
+          // then use onAddTodo to add todo to state
 }
 
 export default BrainDump
